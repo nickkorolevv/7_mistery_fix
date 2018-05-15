48c22b487b5264ea78194ce952667dc2802f5286
nickkorolevv <nick.korolev2011@yandex.ru>
2018-05-15 18:12:43 +0400

    Добавлен исход, когда дискриминант меньше нуля

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,11 +1,25 @@
 from math import sqrt
 
+def get_first_root(major_coef, mid_coef, Discr):
+    first_root = (-mid_coef - sqrt(Discr))/(2 * major_coef)
+    return first_root
+
+
+def get_second_root(major_coef, mid_coef, Discr):
+    second_root = (-mid_coef + sqrt(Discr))/(2 * major_coef)
+    return second_root
+    
 
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
-    root1 = (-b - sqrt(discriminant)) / (2 * a)
-    root2 = (-b + sqrt(discriminant)) / (2 * a)
-    if discriminant == 0:
-        return root1, None
-    else:
-        return root1, root2
+    if discriminant < 0:
+        first_root = None
+        second_root = None
+    elif discriminant == 0:
+        first_root = (-b / (2 * a))
+        second_root = None
+
+    else: 
+        first_root = get_first_root(a, b, discriminant)
+        second_root = get_second_root(a, b, discriminant)
+    return first_root, second_root
