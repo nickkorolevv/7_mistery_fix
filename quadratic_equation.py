bb5d1d63878775d69dd6bf8cbb5bce3f569198ce
nickkorolevv <37769528+nickkorolevv@users.noreply.github.com>
2018-05-15 22:00:41 +0400

    deleted functions

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,25 +1,12 @@
 from math import sqrt
 
-def get_first_root(major_coef, mid_coef, discriminant):
-    first_root = (-mid_coef - sqrt(discriminant))/(2 * major_coef)
-    return first_root
-
-
-def get_second_root(major_coef, mid_coef, discriminant):
-    second_root = (-mid_coef + sqrt(discriminant))/(2 * major_coef)
-    return second_root
-    
-
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
     if discriminant < 0:
-        first_root = None
-        second_root = None
-    elif discriminant == 0:
-        first_root = (-b / (2 * a))
-        second_root = None
-
-    else: 
-        first_root = get_first_root(a, b, discriminant)
-        second_root = get_second_root(a, b, discriminant)
-    return first_root, second_root
+        return None, None
+    root1 = (-b - sqrt(discriminant))/(2*a)
+    root2 = (-b + sqrt(discriminant))/(2*a)
+    if discriminant == 0:
+        return root1, None
+    else:  
+        return root1, root2
